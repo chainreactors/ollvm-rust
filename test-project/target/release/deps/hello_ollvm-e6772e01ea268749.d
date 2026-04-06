3b597bc8b96fae2e2818fc718029db4bd27e08a3
nightRainy <foxfoxfox940@foxmail.com>
2026-04-06 16:47:47 +0800

    fix: remove build artifacts from tracking, update .gitignore

--- test-project/target/release/deps/hello_ollvm-e6772e01ea268749.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/rrr/dev/ollvm-rust/test-project/target/release/deps/hello_ollvm-e6772e01ea268749: src/main.rs
-
-/mnt/c/Users/rrr/dev/ollvm-rust/test-project/target/release/deps/hello_ollvm-e6772e01ea268749.d: src/main.rs
-
-src/main.rs:
--- test-project/target/release/hello-ollvm.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/rrr/dev/ollvm-rust/test-project/target/release/hello-ollvm: /mnt/c/Users/rrr/dev/ollvm-rust/test-project/src/main.rs
